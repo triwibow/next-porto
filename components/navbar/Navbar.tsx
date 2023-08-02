63ef00699e2f034bfb5480c7b87e65e1a4e2b4fa
Tri Wibowo <triw@oninyon.com>
2023-08-02 16:17:37 +0700

    -s

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,44 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import { FaBars } from 'react-icons/fa'
 
 const Navbar:React.FC = () => {
+  const [show, setShow] = useState<boolean>(false);
+
   return (
     <div className='navbar-app container'>
-      <Link href={"/"}>
-        <img 
-          src='/images/logo.svg'
-          className='navbar-app-logo'
-        />
-      </Link>
-      <ul className='navbar-app-nav'>
-        <li className='navbar-app-item'>
-          <Link className='navbar-app-link' href={"#"}>
-            Home
-          </Link>
-        </li>
-        <li className='navbar-app-item'>
-          <Link className='navbar-app-link' href={"#"}>
-            About
-          </Link>
-        </li>
-        <li className='navbar-app-item'>
-          <Link className='navbar-app-link' href={"#"}>
-            Technologies
-          </Link>
-        </li>
-        <li className='navbar-app-item'>
-          <Link className='navbar-app-link' href={"#"}>
-            Contact
-          </Link>
-        </li>
-      </ul>
-      <button className='navbar-app-toggle'>
-        <FaBars 
-          size="19"
-          color="#1F2428"
-        />
-      </button>
+      <div className='navbar-app-brand'>
+        <Link href={"/"}>
+          <img 
+            src='/images/logo.svg'
+            className='navbar-app-logo'
+          />
+        </Link>
+        <button 
+          className='navbar-app-toggle'
+          onClick={() => setShow(!show)}
+        >
+          <FaBars 
+            size="19"
+            color="#1F2428"
+          />
+        </button>
+      </div>
+      <div className={`navbar-app-collapse ${show && ('show')}`}>
+        <ul className='navbar-app-nav'>
+          <li className='navbar-app-item'>
+            <Link className='navbar-app-link' href={"#"}>
+              Home
+            </Link>
+          </li>
+          <li className='navbar-app-item'>
+            <Link className='navbar-app-link' href={"#"}>
+              About
+            </Link>
+          </li>
+          <li className='navbar-app-item'>
+            <Link className='navbar-app-link' href={"#"}>
+              Technologies
+            </Link>
+          </li>
+          <li className='navbar-app-item'>
+            <Link className='navbar-app-link' href={"#"}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
