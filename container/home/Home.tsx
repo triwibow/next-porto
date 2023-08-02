ea72d8e2c716972d4de2344c0582faa9ea7169a2
Tri Wibowo <triw@oninyon.com>
2023-08-02 13:23:25 +0700

    - first commit

---
+++ container/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Alert } from "reactstrap";
+
+const Home:React.FC = () => {
+  return (
+    <Container>
+      {/* <Alert color="primary">
+        Hey! Pay attention.
+      </Alert> */}
+    </Container>
+  )
+}
+
+export default Home;
